f2a0ed9ee86dca5cb794ffedd37ee62d3f26af98
n7.io <w@n7.io>
2012-09-06 19:12:13 +0200

    fixed test Android.mk

--- replace_strings.sh
+++
@@ -1,8 +0,0 @@
-FILES=$(grep -r "android.provider.Calendar" ./ --exclude-dir=".svn" --exclude-dir=".git" | awk -F: '{print $1}' | grep -v 'BCK' | grep "caldav")
-
-for FILE in ${FILES}
-do
-   #cp ${FILE} ${FILE}.BCK;
-   sed 's:android.provider.Calendar:android.provider.CalendarContract:' < ${FILE} > ${FILE}.tmp
-   mv ${FILE}.tmp ${FILE} 
-done
